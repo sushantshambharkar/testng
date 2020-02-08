4e34d8bad00c2961d329028a48e8cfee17a35dba
sushantshambharkar <40300856+sushantshambharkar@users.noreply.github.com>
2020-02-08 16:40:37 -0600

    Add files via upload

---
+++ myTestNG/src/test/java/test/com/CarTest.java
@@ -0,0 +1,63 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package test.com;
+
+import com.itexps.Car;
+import static org.testng.Assert.*;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+/**
+ *
+ * @author Sushant
+ */
+public class CarTest {
+    Car car;
+    
+    
+    public CarTest() {
+    }
+
+    // TODO add test methods here.
+    // The methods must be annotated with annotation @Test. For example:
+    //
+     @Test
+     public void testCar() 
+     {
+         car.setColor("red");
+         car.setMake("honda");
+         car.setVin(1111);
+         
+         assertEquals(1111,car.getVin());
+         assertEquals("red",car.getColor());
+         assertEquals("honda",car.getMake());
+         
+         
+     }
+
+    @BeforeClass
+    public static void setUpClass() throws Exception {
+    }
+
+    @AfterClass
+    public static void tearDownClass() throws Exception {
+    }
+
+    @BeforeMethod
+    public void setUpMethod() throws Exception {
+        
+           car = new Car();
+    }
+
+    @AfterMethod
+    public void tearDownMethod() throws Exception {
+     
+        
+    }
+}
